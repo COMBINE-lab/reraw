eb944f7d3a1aebf0c9e1923c50c68797d8f0e69b
Robert Patro <rob@Roberts-MBP.fios-router.home>
2023-02-26 23:03:03 -0500

    compiles and reads records

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,18 @@ fn main() -> Result<(), std::io::Error> {
     let worker_count = NonZeroUsize::try_from(4).unwrap();
     let mut reader = bgzf::reader::Builder::default().set_worker_count(worker_count).build_from_path(ifn)?;
 
-    let mut reader = bam::Reader::new(reader);
+    let mut reader = bam::Reader::from(reader);
     reader.read_header()?;
     reader.read_reference_sequences()?;
 
     for result in reader.records() {
         let record = result?;
-        println!("{:?}", record);
+        let rn = record.read_name().unwrap();
+        if record.flags().is_first_segment() {
+            println!("alignment for {}, read 1: ", rn);
+        } else  {
+            println!("{:?} ", record);//alignment for {}, read 2: ", rn);
+        }
     }
     Ok(())
 }
